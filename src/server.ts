7d03ab9ad95dc9bfe9f0e7c5c1751ad55dea5bd8
Marcus Davanço <md.daves@gmail.com>
2023-09-13 17:49:19 -0300

    feat: remaining routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getAllPrompsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 export const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*', 
+})
+
 app.register(getAllPrompsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333,  
